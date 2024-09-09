e7589be33d7e3c05f8afa997a868791f5c7b9266
Franco-M-Enzian <emf0000b@gmail.com>
2024-09-09 10:14:37 +0900

    Defined Income Model #5

--- finances/models.py
+++ finances/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from accounts.models import Account
 
-# Create your models here.
+class Income(models.Model):
+    MONTH = [
+        ('january', '1月'),
+        ('february', '2月'),
+        ('march', '3月'),
+        ('april', '4月'),
+        ('may', '5月'),
+        ('june', '6月'),
+        ('july', '7月'),
+        ('august', '8月'),
+        ('september', '9月'),
+        ('october', '10月'),
+        ('november', '11月'),
+        ('december', '12月'),
+    ]
+    CATEGORY = [
+        ("From family", "家庭からの給付"),
+        ("Scholarship", "日本学生支援機構の奨学金"),
+        ("Other scholarship", "日本学生支援機構以外の奨学金"),
+        ("Income from PT job etc.", "アルバイト等収入"),
+        ("Other", "その他"),
+    ]
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    year = models.CharField("年を入力", max_length=4, blank=False)
+    month = models.CharField("月を選択", max_length=10, choices=MONTH, blank=False)
+    category = models.CharField("カテゴリーを選択", max_length=25, choices=CATEGORY, blank=False)
+    amount = models.CharField("金額を入力：円", max_length=7, blank=False)
+
+    def __str__(self):
+        return self.account.username + " : " + self.category + " : " + self.amount + " 円"
