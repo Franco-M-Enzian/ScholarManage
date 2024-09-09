e1272ca54cd9260aff7219866c5de4597b9dea3d
Franco-M-Enzian <emf0000b@gmail.com>
2024-09-09 14:33:33 +0900

    Defined TotalIncome, ExpenseIfNotAlone, ExpenseIfAlone model #10

--- finances/admin.py
+++ finances/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Income
+from .models import TotalIncome, ExpenseIfNotAlone, ExpenseIfAlone
 
-admin.site.register(Income)
+admin.site.register(TotalIncome)
+admin.site.register(ExpenseIfNotAlone)
+admin.site.register(ExpenseIfAlone)
--- finances/models.py
+++ finances/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from accounts.models import Account
 
+
 class Income(models.Model):
     MONTH = [
         ('january', '1月'),
@@ -16,6 +17,16 @@ class Income(models.Model):
         ('november', '11月'),
         ('december', '12月'),
     ]
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    year = models.CharField("年を入力", max_length=4, blank=False)
+    month = models.CharField("月を選択", max_length=10, choices=MONTH, blank=False)
+    amount = models.CharField("金額を入力：円", max_length=7, blank=False)
+
+    class Meta:
+        abstract = True
+
+
+class TotalIncome(Income):
     CATEGORY = [
         ("From family", "家庭からの給付"),
         ("Scholarship", "日本学生支援機構の奨学金"),
@@ -23,11 +34,26 @@ class Income(models.Model):
         ("Income from PT job etc.", "アルバイト等収入"),
         ("Other", "その他"),
     ]
-    account = models.ForeignKey(Account, on_delete=models.CASCADE)
-    year = models.CharField("年を入力", max_length=4, blank=False)
-    month = models.CharField("月を選択", max_length=10, choices=MONTH, blank=False)
     category = models.CharField("カテゴリーを選択", max_length=25, choices=CATEGORY, blank=False)
-    amount = models.CharField("金額を入力：円", max_length=7, blank=False)
 
-    def __str__(self):
-        return self.account.username + " : " + self.category + " : " + self.amount + " 円"
+
+class ExpenseIfNotAlone(Income):
+    CATEGORY = [
+        ("Tuition", "学費"),
+        ("Tuition(Textbook fee etc.)", "修学費"),
+        ("Food expenses", "食費"),
+        ("Communication costs", "通信費"),
+        ("Other", "その他"),
+    ]
+    category = models.CharField("カテゴリーを選択", max_length=26, choices=CATEGORY, blank=False)
+
+
+class ExpenseIfAlone(Income):
+    CATEGORY = [
+        ("Tuition", "学費"),
+        ("Tuition(Textbook fee etc.)", "修学費"),
+        ("Food expenses", "食費"),
+        ("Utility and communication costs", "光熱水料通信費"),
+        ("Other", "その他"),
+    ]
+    category = models.CharField("カテゴリーを選択", max_length=35, choices=CATEGORY, blank=False)
