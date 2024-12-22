2cb6c921716f6c29b672287e15c37028527fc55c
Franco-M-Enzian <emf0000b@gmail.com>
2024-12-23 01:19:59 +0900

    Update settings.py - secret_key

--- config/settings.py
+++ config/settings.py
@@ -26,10 +26,7 @@ env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
