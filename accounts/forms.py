8a3ef7f436197be25313f6abfcb54c247d2cbc07
Franco-M-Enzian <emf0000b@gmail.com>
2024-12-24 15:48:33 +0900

    Changed Birthday Field's widget

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,11 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 from .models import Account
+
+
+class SelectDateWidget(forms.SelectDateWidget):
+    input_type = 'date'
 
 
 class SignupForm(UserCreationForm):
@@ -13,4 +18,7 @@ class SignupForm(UserCreationForm):
             "what_grade",
             "live_with_family",
         )
+        widgets = {
+            'birthday': SelectDateWidget(years=range(1980, 2030)),
+        }
 
